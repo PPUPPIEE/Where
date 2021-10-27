fa0f4fad9872c2b14c1f7becfd55fc3a121d6d07
yoyozapp <peepeebs56@gmail.com>
2021-10-27 15:56:23 +0700

    add route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { FormComponent } from './form/form.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'form', component: FormComponent},
+  {path: 'search', component: SearchComponent}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FormComponent } from './form/form.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FormComponent,
+    NavbarComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
