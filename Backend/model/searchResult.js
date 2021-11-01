6994716a1bed9ee377e03ddec7ff3555e187f0b3
yoyozapp <peepeebs56@gmail.com>
2021-11-01 20:34:58 +0700

    search Not tested

--- Backend/model/searchResult.js
+++ Backend/model/searchResult.js
@@ -5,7 +5,16 @@ const locationSchema = new mongoose.Schema({
     sector: { type: String },
     provinece: { type: String },
     district: { type: String },
-    subdistrict: { type: String },
+    type: { type: String },
+    imageurl: { type: String },
+    opentime: { type: String },
+    closetime: { type: String },
+    toilet: { type: Boolean },
+    parking: { type: Boolean },
+    detail: { type: String },
+    contact: { type: String },
+    security: { type: String },
+    location: { type: String },
     createdAt : {type : Date},
     updatedAt : {type : Date}
 },{
--- Backend/route/search.js
+++ Backend/route/search.js
@@ -3,16 +3,221 @@ const router = require("express").Router();
 const location = require("../model/searchResult");
 
 router.post("/", async (req, res) => {
-  
-  try {
-      const province = await location.aggregate([
-        { $match: { name: { $regex: req.body.name }, sector: req.body.sector } },
-      ]);  
+  //Search name
+  if (
+    req.body.name != "" &&
+    req.body.sector === "" &&
+    req.body.province === "" &&
+    req.body.type === ""
+  ) {
+    try {
+      const searchResult = await location.aggregate([
+        { $match: { name: { $regex: req.body.name } } },
+      ]);
+      res.status(200).json(searchResult);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
 
-   
-    res.status(200).json(province);
-  } catch (err) {
-    res.status(500).json(err);
+  //Search name , sector
+  else if (
+    req.body.name != "" &&
+    req.body.sector != "" &&
+    req.body.province === "" &&
+    req.body.type === ""
+  ) {
+    try {
+      const searchResult = await location.aggregate([
+        {
+          $match: { name: { $regex: req.body.name }, sector: req.body.sector },
+        },
+      ]);
+      res.status(200).json(searchResult);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+
+  //Seach name , province
+  else if (
+    req.body.name != "" &&
+    req.body.sector === "" &&
+    req.body.province != "" &&
+    req.body.type === ""
+  ) {
+    try {
+      const searchResult = await location.aggregate([
+        {
+          $match: {
+            name: { $regex: req.body.name },
+            province: req.body.province,
+          },
+        },
+      ]);
+      res.status(200).json(searchResult);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+
+  //Search name , type
+  else if (
+    req.body.name != "" &&
+    req.body.sector === "" &&
+    req.body.province === "" &&
+    req.body.type != ""
+  ) {
+    try {
+      const searchResult = await location.aggregate([
+        { $match: { name: { $regex: req.body.name }, type: req.body.type } },
+      ]);
+      res.status(200).json(searchResult);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+
+  //Search name , sector , province
+  else if (
+    req.body.name != "" &&
+    req.body.sector != "" &&
+    req.body.province != "" &&
+    req.body.type === ""
+  ) {
+    try {
+      const searchResult = await location.aggregate([
+        {
+          $match: {
+            name: { $regex: req.body.name },
+            sector: req.body.sector,
+            province: req.body.province,
+          },
+        },
+      ]);
+      res.status(200).json(searchResult);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+
+  //Search name , sector , type
+  else if (
+    req.body.name != "" &&
+    req.body.sector != "" &&
+    req.body.province === "" &&
+    req.body.type != ""
+  ) {
+    try {
+      const searchResult = await location.aggregate([
+        { $match: { name: { $regex: req.body.name }, type: req.body.type } },
+      ]);
+      res.status(200).json(searchResult);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+
+  //Search sector
+  else if (
+    req.body.name === "" &&
+    req.body.sector != "" &&
+    req.body.province === "" &&
+    req.body.type === ""
+  ) {
+    try {
+      const searchResult = await location.aggregate([
+        { $match: { sector: req.body.sector } },
+      ]);
+      res.status(200).json(searchResult);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+
+  //Search sector , province
+  else if (
+    req.body.name === "" &&
+    req.body.sector != "" &&
+    req.body.province != "" &&
+    req.body.type === ""
+  ) {
+    try {
+      const searchResult = await location.aggregate([
+        { $match: { sector: req.body.sector, province: req.body.province } },
+      ]);
+      res.status(200).json(searchResult);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+
+  //Search sector , type
+  else if (
+    req.body.name === "" &&
+    req.body.sector != "" &&
+    req.body.province === "" &&
+    req.body.type != ""
+  ) {
+    try {
+      const searchResult = await location.aggregate([
+        { $match: { sector: req.body.sector, type: req.body.type } },
+      ]);
+      res.status(200).json(searchResult);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+
+  //Search sector , province , type
+  else if (
+    req.body.name === "" &&
+    req.body.sector != "" &&
+    req.body.province != "" &&
+    req.body.type != ""
+  ) {
+    try {
+      const searchResult = await location.aggregate([
+        { $match: { sector: req.body.sector, type: req.body.type } },
+      ]);
+      res.status(200).json(searchResult);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+
+  //Search province
+  else if (
+    req.body.name === "" &&
+    req.body.sector === "" &&
+    req.body.province != "" &&
+    req.body.type === ""
+  ) {
+    try {
+      const searchResult = await location.aggregate([
+        { $match: { province: req.body.sector } },
+      ]);
+      res.status(200).json(searchResult);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+
+  //Search province , type
+  else if (
+    req.body.name === "" &&
+    req.body.sector === "" &&
+    req.body.province != "" &&
+    req.body.type != ""
+  ) {
+    try {
+      const searchResult = await location.aggregate([
+        { $match: { province: req.body.sector, type: req.body.type } },
+      ]);
+      res.status(200).json(searchResult);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   }
 });
 
