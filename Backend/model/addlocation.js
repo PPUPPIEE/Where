628f9259a2a466c8f46bb8bb5662c762b60fe761
Aniwatchuychay <Aniwat.Chuychay@gmail.com>
2021-11-01 16:49:34 +0700

    Update Addlocation เพิ่มประเภท และอื่นๆ

--- Backend/model/addlocation.js
+++ Backend/model/addlocation.js
@@ -5,11 +5,19 @@ const AddlocationSchema = new mongoose.Schema({
     sector: { type: String },
     provinece: { type: String },
     district: { type: String },
-    subdistrict: { type: String }
-},{
-    collation: "addlocation",
-    timestamps:true
+    type: { type: String },
+    imageurl: { type: String },
+    opentime: { type: String },
+    closetime: { type: String },
+    toilet: { type: Boolean },
+    parking: { type: Boolean },
+    detail: { type: String },
+    contact: { type: String },
+    security: { type: String },
+    location: { type: String }
+}, {
+    timestamps: true
 }
-    );
+);
 
-    module.exports = mongoose.model('Addlocation',AddlocationSchema);
+module.exports = mongoose.model('Addlocation', AddlocationSchema);
--- Backend/route/postAddlocation.js
+++ Backend/route/postAddlocation.js
@@ -9,15 +9,24 @@ router.post("/", (req, res) => {
         sector: req.body.sector,
         provinece: req.body.provinece,
         district: req.body.district,
-        subdistrict: req.body.subdistrict
+        type: req.body.type,
+        imageurl: req.body.imageurl,
+        opentime: req.body.opentime,
+        closetime: req.body.closetime,
+        toilet: req.body.toilet,
+        parking: req.body.parking,
+        detail: req.body.detail,
+        contact: req.body.contact,
+        security: req.body.security,
+        location: req.body.location
     });
     post.save()
-    .then(data=>{
-        res.json(data)
-    })
-    .catch(err =>{
-        res.json({message: err})
-    });
+        .then(data => {
+            res.json({message:"ลงสถานที่เสร็จสิ้น กรุณารอทีมงานตรวจสอบ"})
+        })
+        .catch(err => {
+            res.json({ message: err })
+        });
 });
 
 module.exports = router;
