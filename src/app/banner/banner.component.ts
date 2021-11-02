e630e53d5c49bd114aef4db1ccb73fb09813c7a3
Pathompong ThonChinda <niceooza@gmail.com>
2021-11-02 14:38:11 +0700

    new function province

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import  data  from 'src/assets/json/province.json';
+import data from 'src/assets/json/province.json';
 
 @Component({
   selector: 'app-banner',
@@ -7,39 +7,47 @@ import  data  from 'src/assets/json/province.json';
   styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent implements OnInit {
-  
-  provinceList:{name:string,sector:string}[] = data;
+
+  provinceList: { name: string, sector: string }[] = data;
   newProvinceList: string[] = [];
-  sector: string[] = ["เหนือ","ตะวันออกเฉียงเหนือ","กลาง","ใต้","ตะวันออก","ตะวันตก"];
-  province:string = 'จังหวัด';
-  sectorName:string = 'ภาค';
-  ngGet:string = 'true';
-  
+  sector: string[] = ["เหนือ", "ตะวันออกเฉียงเหนือ", "กลาง", "ใต้", "ตะวันออก", "ตะวันตก"];
+  province: string = 'จังหวัด';
+  sectorName: string = 'ภาค';
+  ngGet: string = 'true';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  changeProvince(value: string){
+  changeProvince(value: string) {
     this.province = value;
     this.provinceList.forEach(item => {
-      if(item.name == this.province){
+      if (item.name == this.province) {
         this.sectorName = item.sector;
       }
     });
 
 
   }
-  changeSector(value: string){
+  changeSector(value: string) {
     this.sectorName = value;
     this.province = 'จังหวัด';
     this.newProvinceList =[];
-    this.provinceList.forEach(item =>{
-      if(item.sector === this.sectorName)
-      this.newProvinceList.push(item.name)
+    this.provinceList.forEach(item => {
+      if (item.sector === this.sectorName)
+        this.newProvinceList.push(item.name)
     });
   }
 
-  
+  addProvince(){
+    if (this.sectorName == "ภาค") {
+      this.provinceList.forEach(item => {
+        this.newProvinceList.push(item.name)
+      });
+    }
+  }
 
 }
+
+
