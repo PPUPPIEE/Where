1a8fc278764496e31b89af8d5b9e9d72da66cba8
Penquinnz <oraiser28@gmail.com>
2021-11-07 00:26:11 +0700

    Update Searchbar

---
+++ src/app/components/seachbar/province.ts
@@ -0,0 +1,5 @@
+export interface getProvince{
+    id: number;
+    sector: string;
+    name: string;
+}
---
+++ src/app/components/seachbar/seachbar.component.ts
@@ -0,0 +1,95 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ShareService } from 'src/app/services/share.service';
+
+import { getProvince } from './province';
+
+
+@Component({
+  selector: 'app-seachbar',
+  templateUrl: './seachbar.component.html',
+  styleUrls: ['./seachbar.component.scss']
+})
+export class SeachbarComponent implements OnInit {
+
+  provinceList: getProvince[] = [];
+  newProvinceList: string[] = [];
+  province: string = 'จังหวัด';
+  sector: string[] = ["เหนือ", "ตะวันออกเฉียงเหนือ", "กลาง", "ใต้", "ตะวันออก", "ตะวันตก"];
+  sectorName: string = 'ภาค';
+  typeList: string[] = ["ร้านอาหาร", "วัฒนธรรม", "แหล่งท่องเที่ยวตามธรรมชาติ"];
+  type = "ประเภท";
+
+  constructor(
+    private http: HttpClient,
+    public share: ShareService) { }
+
+  ngOnInit(): void {
+    this.getProvince();
+  }
+
+  getProvince() {
+    this.http.get<getProvince[]>('https://dry-dawn-24095.herokuapp.com/api/province').subscribe(
+      response => { this.provinceList = response }
+    )
+  }
+
+  changeProvince(value: string) {
+    this.province = value;
+    // this.provinceList.forEach(item => {
+    //   if (item.name == this.province) {
+    //     this.sectorName = item.sector;
+    //   }
+    // });
+
+
+  }
+  changeSector(value: string) {
+    this.sectorName = value;
+    this.province = 'จังหวัด';
+    this.newProvinceList = [];
+    this.provinceList.forEach(item => {
+      if (item.sector === this.sectorName)
+        this.newProvinceList.push(item.name)
+    });
+  }
+
+  changeType(value: string){
+    this.type = value;
+  }
+
+  addProvince() {
+    if (this.sectorName == "ภาค") {
+      this.provinceList.forEach(item => {
+        this.newProvinceList.push(item.name)
+      });
+    }
+  }
+
+  postSearch(value:string) {
+    //   this.http.post('https://dry-dawn-24095.herokuapp.com/api/search',{
+    //     "name":value,
+    //     "sector" : "",
+    //     "province" : "",
+    //     "type" : ""
+    // }).subscribe(Response => console.log(Response))
+    if(this.type === "ประเภท"){this.type =""};
+    if(this.sectorName === "ภาค"){this.sectorName =""};
+    if(this.province === "จังหวัด"){this.province =""};
+    if(this.type === ""){this.type ="ประเภท"};
+    if(this.sectorName === ""){this.sectorName ="ภาค"};
+    if(this.province === ""){this.province ="จังหวัด"};
+
+    this.share.searchName = value;
+    this.share.searchSector = this.sectorName;
+    this.share.searchProvince = this.province;
+    this.share.searchType = this.type;
+
+    console.log(this.type , this.sectorName, this.province);
+
+  }
+
+}
+
+
+
