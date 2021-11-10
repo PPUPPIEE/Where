b5ac34080c5d64a7e65e0b5ca898c68d5e0ce96c
Pathompong ThonChinda <niceooza@gmail.com>
2021-11-10 15:49:09 +0700

    array img

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -7,42 +7,42 @@ import { ShareService } from 'src/app/services/share.service';
   styleUrls: ['./detail.component.scss']
 })
 export class DetailComponent implements OnInit {
-  Parking : string ="";
-  Toilet : string = "";
-  
+  Parking: string = "";
+  Toilet: string = "";
+
 
   constructor(public share: ShareService) {
-   }
+  }
 
   ngOnInit(): void {
 
-    if( window.localStorage )
-    {
-      if( !localStorage.getItem('firstLoad') )
-      {
+    if (window.localStorage) {
+      if (!localStorage.getItem('firstLoad')) {
         localStorage['firstLoad'] = true;
         window.location.reload();
-      }  
+      }
       else
         localStorage.removeItem('firstLoad');
-    }  
+    }
 
-if(this.share.detailParking == "true"){ this.Parking ="ที่จอดรถ"}
-if(this.share.detailToilet == "true"){ this.Toilet ="ห้องน้ำ"}
-if(this.share.detailToilet == "false" && this.share.detailParking == "false") {this.Parking = "-"}
+    console.log(this.share.detailImageurl)
+
+    if (this.share.detailParking == "true") { this.Parking = "ที่จอดรถ" }
+    if (this.share.detailToilet == "true") { this.Toilet = "ห้องน้ำ" }
+    if (this.share.detailToilet == "false" && this.share.detailParking == "false") { this.Parking = "-" }
 
 
-if(this.share.detailName=="null"){ this.share.detailName = "-"}
-if(this.share.detailProvine=="null"){ this.share.detailProvine = "-"}
-if(this.share.detailSector=="null"){ this.share.detailSector = "-"}
-if(this.share.detailDistrict=="null"){ this.share.detailDistrict = "-"}
-if(this.share.detailType=="null"){ this.share.detailType = "-"}
-if(this.share.detailOpentime=="null"){ this.share.detailOpentime = "-"}
-if(this.share.detailClosetime=="null"){ this.share.detailClosetime = "-"}
-if(this.share.detailDetail=="null"){ this.share.detailDetail = "-"}
-if(this.share.detailContact=="null"){ this.share.detailContact = "-"}
-if(this.share.detailSecurity=="null"){ this.share.detailSecurity = "-"}
-if(this.share.detailLocation=="null"){ this.share.detailLocation = "-"}
+    if (this.share.detailName == "null") { this.share.detailName = "-" }
+    if (this.share.detailProvine == "null") { this.share.detailProvine = "-" }
+    if (this.share.detailSector == "null") { this.share.detailSector = "-" }
+    if (this.share.detailDistrict == "null") { this.share.detailDistrict = "-" }
+    if (this.share.detailType == "null") { this.share.detailType = "-" }
+    if (this.share.detailOpentime == "null") { this.share.detailOpentime = "-" }
+    if (this.share.detailClosetime == "null") { this.share.detailClosetime = "-" }
+    if (this.share.detailDetail == "null") { this.share.detailDetail = "-" }
+    if (this.share.detailContact == "null") { this.share.detailContact = "-" }
+    if (this.share.detailSecurity == "null") { this.share.detailSecurity = "-" }
+    if (this.share.detailLocation == "null") { this.share.detailLocation = "-" }
 
   }
 
--- src/app/services/share.service.ts
+++ src/app/services/share.service.ts
@@ -26,7 +26,7 @@ export class ShareService {
   detailSector= localStorage.getItem("detailSector");
   detailDistrict= localStorage.getItem("detailDistrict");
   detailType= localStorage.getItem("detailType");
-  detailImageurl= JSON.parse(localStorage.getItem("imageUrl") || "[]");
+  detailImageurl= JSON.parse(localStorage.getItem("detailImageurl") || "[]");
   detailOpentime= localStorage.getItem("detailOpentime");
   detailClosetime= localStorage.getItem("detailClosetime");
   detailToilet= localStorage.getItem("detailToilet");
