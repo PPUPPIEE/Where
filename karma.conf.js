22b64f94e0c6e7f3c547f93d0bdc8c43723ac242
yoyozapp <peepeebs56@gmail.com>
2021-11-17 23:26:25 +0700

    Final

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/nivethk'),
+      dir: require('path').join(__dirname, './coverage/where'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'nivethk'`, () => {
+  it(`should have as title 'where'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('nivethk');
+    expect(app.title).toEqual('where');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('nivethk app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('where app is running!');
   });
 });
